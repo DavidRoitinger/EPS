8de0cbbe2bf6a39aed45f5ab905905a1638c582d
KalichVivil <roitingerD190181@sus.htl-grieskirchen.at>
2022-09-19 11:21:52 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public interface PrimeSieve {
+
+
+
+
+    }
+
+    public boolean isPrime( int p ) {
+        return false;
+    }
+
+    public void printPrimes(){
+
+    }
 }
